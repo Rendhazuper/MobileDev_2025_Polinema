17a29016229fffd5561f26489131cf029d356f07
Rendha <rendhazuper@gmail.com>
2025-10-15 11:42:28 +0700

    pengerjaan jobsheet 6

---
+++ Jobsheet_6/Project_Layout/README.md
@@ -0,0 +1,31 @@
+# Laporan Praktikum 6
+Nama    : Rendha Putra Rahmadya
+Kelas   : TI - 3E
+NIM     : 2341720010
+Absen   : 26
+
+## Praktikum 1 : Layout dan Navigasi
+
+Menambahkan kode berikut 
+![alt text](image.png)
+
+Dengan hasil seperti berikut 
+![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+
+## Praktikum 2 : Implementasi Button Row
+![alt text](image-3.png)
+
+## Praktikum 3 : Implementasi text section
+Penambahan text section 
+![alt text](image-4.png)
+
+## Praktikum 4 : Implementasi image section
+![alt text](image-5.png)
+
+## Tugas 1 : Implementasi flutter docs
+![alt text](image-6.png)
+
+
---
+++ Jobsheet_6/belanja/lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/item.dart';
+import '../widgets/product_card.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(
+      name: 'Sugar',
+      price: 5000,
+      image: 'assets/sugar.png', // Changed from 'sugar.png'
+      stock: 10,
+      rating: 4.5,
+    ),
+    Item(
+      name: 'Salt',
+      price: 20000,
+      image: 'assets/salt.png', // Changed from 'salt.png'
+      stock: 5,
+      rating: 4.0,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Shopping List')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: items.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.72,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return ProductCard(
+              item: item,
+              onTap: () => context.pushNamed('item', extra: item),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
